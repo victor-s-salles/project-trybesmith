160111782f8d7059ba40d7b19e8b28c1d2b14c56
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-03-14 15:35:47 -0300

    Correção no nome de model product

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,5 @@
 import { IProduct } from '../interfaces';
-import productModel from '../models/Product.model';
+import productModel from '../models/product.model';
 
 const getAll = async (): Promise<IProduct[]> => {
   const products = await productModel.getAll();
